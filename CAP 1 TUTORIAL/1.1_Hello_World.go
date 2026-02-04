df4952e526c4197aa7c6eeeb2e1871c2a075392a
Inacio Araripe <araripeinacio@gmail.com>
2026-02-04 08:18:56 -0300

    Hello World basico

---
+++ CAP 1 TUTORIAL/1.1_Hello_World.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//função principal, responsavel por receber e enviar dados(input e output)
+//Nesse exemplo, servindo apenas como saida de dados
+
+func main() {
+
+	//fmt importado. Responsavel por fazer essa saida(dar o print)
+	//Println padrão, assim como no java, c e linguagens semelhantes
+	fmt.Println("Hello World!")
+}
